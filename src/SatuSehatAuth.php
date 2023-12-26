8eb78abd60f03796ea13b512a6cd55167b8c1127
ictsatri <triwaskito.ictsa@gmail.com>
2023-12-26 15:57:08 +0700

    initial commit

--- src/SatuSehatAuth.php
+++ src/SatuSehatAuth.php
@@ -2,12 +2,82 @@
 
 namespace satusehat\integration;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request;
+
 class SatuSehatAuth
 {
+	public string $auth_url;
+	
+	public string $base_url;
+	
+	public string $client_id;
+	
+	public string $client_secret;
+	
+	public string $organization_id;
 
   public function __construct()
   {
-    
+	  $this->auth_url        = xxxx;
+	  $this->base_url        = xxxx;
+	  $this->client_id       = xxxx;
+	  $this->client_secret   = xxxx;
+	  $this->organization_id = xxxx;
+	  
+	  if ($this->organization_id == null) {
+		  return 'Add your organization_id at environment first';
+	  }
   }
+	
+	public function token()
+	{
+		$token = SatusehatToken::where('environment', getenv('SATUSEHAT_ENV'))->orderBy('created_at', 'desc')
+		                       ->where('created_at', '>', now()->subMinutes(50))->first();
+		
+		if ($token) {
+			return $token->token;
+		}
+		
+		$client = new Client();
+		
+		$headers = [
+		 'Content-Type' => 'application/x-www-form-urlencoded',
+		];
+		$options = [
+		 'form_params' => [
+			'client_id' => $this->client_id,
+			'client_secret' => $this->client_secret,
+		 ],
+		];
+		
+		// Create session
+		$url = $this->auth_url.'/accesstoken?grant_type=client_credentials';
+		$request = new Request('POST', $url, $headers);
+		
+		try {
+			$res = $client->sendAsync($request, $options)->wait();
+			$contents = json_decode($res->getBody()->getContents());
+			
+			if (isset($contents->access_token)) {
+				SatusehatToken::create([
+				                        'environment' => getenv('SATUSEHAT_ENV'),
+				                        'token' => $contents->access_token,
+				                       ]);
+				
+				return $contents->access_token;
+			} else {
+				// return $this->respondError($oauth2);
+				return null;
+			}
+		} catch (ClientException $e) {
+			// error.
+			$res = json_decode($e->getResponse()->getBody()->getContents());
+			$issue_information = $res->issue[0]->details->text;
+			
+			return $issue_information;
+		}
+	}
 
 }
